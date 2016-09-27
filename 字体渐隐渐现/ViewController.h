c9ef153fca31fd2614d7ba60fba37d2c9195cb72
残月 <632342527@qq.com>
2016-09-27 15:17:28 +0800

    Initial Commit

---
+++ 字体渐隐渐现/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  字体渐隐渐现
+//
+//  Created by 王奥东 on 16/9/27.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 字体渐隐渐现/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  字体渐隐渐现
+//
+//  Created by 王奥东 on 16/9/27.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
